fb14f460f00fcc764e1df7d8b7384cdfacb32af1
Louis Amedro <osiris62100@hotmail.fr>
2025-06-01 12:26:30 +0200

    Division des pages/styles

--- script/script.js
+++ script/script.js
@@ -1,3 +1,10 @@
+fetch('/page/header.html')
+.then(response => response.text())
+.then(data => document.getElementById('header').innerHTML = data);
+fetch('/page/footer.html')
+.then(response => response.text())
+.then(data => document.getElementById('footer').innerHTML = data);
+
 window.addEventListener('scroll', function() {
     const header = document.querySelector('#menu');
     if (window.scrollY > 500) {
